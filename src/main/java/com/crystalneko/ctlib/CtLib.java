38adee4e4e2f4fa86fdbd70d6d9d6994314f5db8
CrystalNeko <minecrafthetun@outlook.com>
2023-10-03 10:56:00 +0800

    新增数据库操作

--- src/main/java/com/crystalneko/ctlib/CtLib.java
+++ src/main/java/com/crystalneko/ctlib/CtLib.java
@@ -1,12 +1,20 @@
 package com.crystalneko.ctlib;
 
+import com.crystalneko.ctlib.sql.sqlite;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.sql.SQLException;
 
 public final class CtLib extends JavaPlugin {
     @Override
     public void onEnable() {
         System.out.println("国庆&中秋快乐！");
-
+        //创建sqlite连接
+        try {
+            sqlite.createConnection();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
     }
 
 
