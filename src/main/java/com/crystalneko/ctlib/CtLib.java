3ee8177de06b3fc2463e95737d6b6cc7555e3d69
CrystalNeko <minecrafthetun@outlook.com>
2023-11-07 22:54:32 +0800

    修复屎山第一步
    我想尝试把屎山修复一下

--- src/main/java/com/crystalneko/ctlib/CtLib.java
+++ src/main/java/com/crystalneko/ctlib/CtLib.java
@@ -4,10 +4,13 @@ import com.crystalneko.ctlib.command.ctEcomonyCommand;
 import com.crystalneko.ctlib.ecomony.playerEcomony;
 import com.crystalneko.ctlib.event.onPlayerJoin;
 import com.crystalneko.ctlib.sql.sqlite;
-import com.crystalneko.ctlib.sql.mysql;
+import com.crystalneko.ctlibPublic.File.YamlConfiguration;
+import com.crystalneko.ctlibPublic.sql.mysql;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.sql.SQLException;
 
 public final class CtLib extends JavaPlugin {
@@ -30,8 +33,16 @@ public final class CtLib extends JavaPlugin {
         // -------------------------------------------------------------初始化------------------------------------------------------
 
         if(getConfig().getBoolean("mysql.enable")) {
+            Path configPath = Path.of("plugins/ctLib/config.yml");
+            YamlConfiguration config = null;
+            try {
+                config = new YamlConfiguration(configPath);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
             //创建mysql连接
-            this.mysql = new mysql(this);
+            this.mysql = new mysql(config);
+            new com.crystalneko.ctlib.sql.mysql(this);
             getCommand("ctecomony").setExecutor(new ctEcomonyCommand(this));
             this.playerEcomony = new playerEcomony(this);
         }
