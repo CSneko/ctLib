c219867db50a6d342ac0b83c41d09b0095ca931b
CSneko <minecrafthetun@outlook.com>
2024-03-24 15:26:58 +0800

    支持NukkitX

---
+++ src/main/java/org/cneko/ctlib/bootstrap/NukkitBootstrap.java
@@ -0,0 +1,32 @@
+package org.cneko.ctlib.bootstrap;
+
+import cn.nukkit.plugin.PluginBase;
+import net.byteflux.libby.LibraryManager;
+import net.byteflux.libby.NukkitLibraryManager;
+import org.cneko.ctlib.plugin.util.LibrariesLoader;
+
+import static org.cneko.ctlib.common.util.LocalDataBase.Connections.sqlite;
+
+public class NukkitBootstrap extends PluginBase {
+    @Override
+    public void onLoad() {
+
+    }
+
+    @Override
+    public void onEnable() {
+        // 初始化load
+        LibraryManager libraryManager;
+        libraryManager = new NukkitLibraryManager(this);
+        LibrariesLoader.setManager(libraryManager);
+        // 加载依赖项
+        LibrariesLoader.load("org.xerial", "sqlite-jdbc", "3.41.2.2");
+        LibrariesLoader.load("org.jetbrains.kotlin", "kotlin-stdlib", "2.0.0-Beta1");
+
+    }
+
+    @Override
+    public void onDisable() {
+        sqlite.disconnect();
+    }
+}
